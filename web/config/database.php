935b08d6a4670c1ba0a0f20d37cc2ad4a139d4fe
sergiopalbau <sergio.palbau@educa.jcyl.es>
2018-12-25 18:56:26 +0100

    se añade control error en conexion a la bdd

--- web/config/database.php
+++ web/config/database.php
@@ -4,6 +4,13 @@
      public static function conectar () {
          $conexion = new mysqli ("localhost", "root", "","signin");
          $conexion->query("SET NAMES 'utf8'");
+         if ($conexion->connect_errno) {
+            echo "Lo sentimos, este sitio web está experimentando problemas.";
+            echo "Error: Fallo al conectarse a MySQL debido a: \n";
+            echo "Errno: " . $conexion->connect_errno . "\n";
+            echo "Error: " . $conexion->connect_error . "\n";
+            exit;
+        }
          return $conexion;
      }
  }
