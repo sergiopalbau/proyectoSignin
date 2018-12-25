e3e71dbd5f02f03c71344b59613f0ef7ebe6b5e2
sergiopalbau <sergio.palbau@educa.jcyl.es>
2018-12-25 13:56:48 +0100

    pagina index normal, sin mvc

--- web/index.php
+++ web/index.php
@@ -1,32 +1,3 @@
-<?php 
-    require_once 'autoload.php';
-    if (isset($_GET['controller']))
-    {
-        $nombre_controlador = $_GET ['controller'].'Controller';
-    }else{
-        index();
-        exit();
-    }
-    
-    if (class_exists ($nombre_controlador)){
-        $controlador = new $nombre_controlador();
-        
-        if (isset ($_GET['action']) && method_exists($controlador, $_GET['action'])){
-            $action = $_GET['action'];
-            $controlador->$action();
-        }else {
-            echo "La pagina que buscas no existe";
-        }
-    }else {
-        echo "La pagina que buscas no existe";
-    }
-    
-?>
-
-<?php
-    function index () {
-       
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -68,4 +39,3 @@
 	
 </body>
 </html>
-    <?php } ?>
