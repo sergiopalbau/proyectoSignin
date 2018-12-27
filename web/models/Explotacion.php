cbbee23ef436d2b650f6c00fbf15ef800320d23b
sergiopalbau <sergio.palbau@educa.jcyl.es>
2018-12-27 23:36:37 +0100

    añadido metodo editar.

--- web/models/Explotacion.php
+++ web/models/Explotacion.php
@@ -33,5 +33,24 @@ class Explotacion extends ModeloBase {
         echo $sql;
         return true;
     }
+    
+    function edit ()
+    {
+        $sql= "UPDATE explotacion SET id_explotacion = '{$this->id}', municipio = '{$this->municipio}' WHERE id_explotacion = '{$this->id}'";
+        $this->db->query($sql);
+        echo  $sql;
+        return true;
+    }
+    
+    
+    public function conseguirId (){
+        $query = $this->db->query ("SELECT * FROM explotacion WHERE id_explotacion = '{$this->id}'");
+        if ($query->num_rows === 1)
+        {
+            $this->datos= $query->fetch_assoc();
+            
+        }
+        return $this->datos;
+    }
 
 }// fin clase
