61e75e0fec27e4e103c28a89ce653017a02934a0
sergiopalbau <sergio.palbau@educa.jcyl.es>
2018-12-29 02:14:19 +0100

    acceso controller .>index
    se define el controlador para la accion index

---
+++ web/controllers/AccesoController.php
@@ -0,0 +1,104 @@
+<?php
+
+class AccesoController {
+     // metodo por defecto, muestra una tabla con todos los valores de la bbdd
+    function index () {
+        echo "acceso controller index";
+        require 'models/Acceso.php';
+        
+        $acceso = new Acceso();
+        $datos =$acceso->conseguirTodos ('acceso');
+        
+        require_once 'views/acceso/index.phtml';
+       
+        
+    }
+    
+    function nuevo () {
+       require_once 'views/explotacion/nuevo.phtml';
+    }
+
+    function add () {
+    
+        if ($_POST){
+            var_dump($_POST);
+            include_once 'models/Explotacion.php';
+            if (isset($_POST['id_explotacion']) && isset ($_POST['municipio'])){
+
+                    $explotacion = new Explotacion;
+                    $explotacion->setId ($_POST['id_explotacion']);
+                    $explotacion->setMunicipio($_POST['municipio']);
+                    
+                    if ($explotacion->add()){
+                        header ("location: ?controller=explotacion&action=index");
+                    }
+             }else {
+                echo "no hay envio post, nada que hacer";
+                exit;
+            }
+        }
+    }
+    
+    // abrir ventana editar...
+    function nuevoeditar () {
+        if ( (($_SESSION['rol'] == 'superAdmin') || ($_SESSION['rol'] == 'Admin')) && isset($_GET['dato']) ){
+            include_once 'models/Explotacion.php';
+            $explotacion = new Explotacion;   
+            $explotacion->setId ($_GET['dato']);
+            $consulta= $explotacion->conseguirId();
+            require_once 'views/explotacion/nuevo.phtml';
+        }else{
+            Echo "No tiene suficientes privilegios para llevar a cabo esta operacion";
+        }
+        
+    }
+    // llamada a model edit
+    function edit (){
+        if ($_POST){
+            var_dump ($_POST);
+            include_once 'models/Explotacion.php';
+            $explotacion = new Explotacion;   
+            $explotacion->setId ($_POST['id_explotacion']);
+            $explotacion->setMunicipio($_POST['municipio']);
+            if ($explotacion->edit()){
+                header ("location: ?controller=explotacion&action=index");
+            }else{
+                echo "no se pudo completar la operacion.";
+                header ("Refresh:5; url=?controller=explotacion&action=index");
+            }
+        }else {
+            echo "Faltan datos.";
+        }
+        
+        
+    }
+    
+    function eliminar () {
+        if ( (($_SESSION['rol'] == 'superAdmin') || ($_SESSION['rol'] == 'Admin')) && isset($_GET['dato']) ){
+            include_once 'models/Explotacion.php';
+            $explotacion = new Explotacion;   
+            $explotacion->setId ($_GET['dato']);
+            $consulta= $explotacion->conseguirId();
+              require_once 'views/explotacion/eliminar.phtml';
+        }else{
+            Echo "No tiene suficientes privilegios para llevar a cabo esta operacion";
+        }
+        
+    }
+    
+    function delete () {
+        if ($_POST){
+            var_dump ($_POST);
+            include_once 'models/Explotacion.php';
+            $explotacion = new Explotacion;   
+            $explotacion->setId ($_POST['id_explotacion']);
+            $explotacion->setMunicipio($_POST['municipio']);
+            if ($explotacion->delete()){
+                        header ("location: ?controller=explotacion&action=index");
+                    }
+        }else {
+            echo "Faltan datos.";
+        }
+    }
+} //fin class
+
