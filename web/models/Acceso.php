2e0ee8fc1bd448d4401d1d8c4ed70eeb820188a8
sergiopalbau <sergio.palbau@educa.jcyl.es>
2018-12-29 01:59:32 +0100

    definir modelo ACCESO

--- web/models/Acceso.php
+++ web/models/Acceso.php
@@ -3,36 +3,84 @@
 require_once 'ModeloBase.php';
 class Acceso extends ModeloBase {
 
-    private $id;
-    private $municipio;
-  
+    private $id_acceso;
+    private $id_rol3;
+    private $nombre;
+    private $password;
+    private $email;
+    private $telefono;
+    private $id_explotacion3;
+
     
-     public function __construct() {
+   // '{$this->id_acceso}', '{$this->id_rol3}', '{$this->nombre}', '{$this->password}', '{$this->email}', '{$this->telefono}', '{$this->id_explotacion3}'
+    public function __construct() {
         parent::__construct();
             
     }
     
-    function getId() {
-        return $this->id;
+    function getId_acceso() {
+        return $this->id_acceso;
     }
 
-    function getMunicipio() {
-        return $this->municipio;
+    function getId_rol3() {
+        return $this->id_rol3;
     }
 
-    function setId($id) {
-        $this->id = $id;
+    function getNombre() {
+        return $this->nombre;
     }
 
-    function setMunicipio($municipio) {
-        $this->municipio = $municipio;
+    function getPassword() {
+        return $this->password;
     }
-    /**
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getTelefono() {
+        return $this->telefono;
+    }
+
+    function getId_explotacion3() {
+        return $this->id_explotacion3;
+    }
+
+    function setId_acceso($id_acceso) {
+        $this->id_acceso = $id_acceso;
+    }
+
+    function setId_rol3($id_rol3) {
+        $this->id_rol3 = $id_rol3;
+    }
+
+    function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    function setPassword($password) {
+        $this->password = $password;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setTelefono($telefono) {
+        $this->telefono = $telefono;
+    }
+
+    function setId_explotacion3($id_explotacion3) {
+        $this->id_explotacion3 = $id_explotacion3;
+    }
+
+       /**
      * añade el objeto a la bbdd
      * @return boolean
      */
     function add() {
-        $sql = "INSERT INTO explotacion VALUES ('{$this->id}','{$this->municipio}')";
+        $sql = "INSERT INTO acceso VALUES ('{$this->id_acceso}', '{$this->id_rol3}', '{$this->nombre}', '{$this->password}', '{$this->email}', '{$this->telefono}',
+    '{$this->id_explotacion3}')";
         $query= $this->db->query($sql);
         return $query;
     }
@@ -42,7 +90,7 @@ class Acceso extends ModeloBase {
      */
     function edit ()
     {
-        $sql= "UPDATE explotacion SET id_explotacion = '{$this->id}', municipio = '{$this->municipio}' WHERE id_explotacion = '{$this->id}'";
+        $sql= "UPDATE acceso SET id_acceso = '{$this->id_acceso}', id_rol3 = '{$this->id_rol3}', nombre ='{$this->nombre}', password ='{$this->password}', email= '{$this->email}', telefono ='{$this->telefono}', id_explotacion3 = '{$this->id_explotacion3}' WHERE id_acceso = '{$this->id_acceso}'";
         $query=$this->db->query($sql);
         echo  $sql;
         return $query;
@@ -52,7 +100,7 @@ class Acceso extends ModeloBase {
      * @return boolean
      */
     function delete (){
-        $sql= "DELETE FROM explotacion WHERE id_explotacion = '{$this->id}'";
+        $sql= "DELETE FROM acceso WHERE id_acceso = '{$this->id_acceso}'";
         $query=$this->db->query($sql);
         echo  $sql;
         return $query;
@@ -63,7 +111,7 @@ class Acceso extends ModeloBase {
      * @return array()
      */
     public function conseguirId (){
-        $query = $this->db->query ("SELECT * FROM explotacion WHERE id_explotacion = '{$this->id}'");
+        $query = $this->db->query ("SELECT * FROM acceso WHERE id_acceso = '{$this->id_acceso}'");
         if ($query->num_rows === 1)
         {
             $this->datos= $query->fetch_assoc();
